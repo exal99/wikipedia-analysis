b0bef4ebc53abed249a81ded2441894a8fe51210
Alexander Simko <alexander@simko.se>
2018-09-01 13:01:57 +0200

    Improved download script

--- download_db.sh
+++ download_db.sh
@@ -3,16 +3,12 @@
 # Usage: download_db.sh LANG DATE FILE
 
 
-
-
-#echo $CUR_DIR
-#echo "$CUR_DIR/$OUT_DIR"
-#echo $URL
-
 CUR_DIR=`pwd`
 OUT_DIR="database"
 
 function download_file() {
+	echo "[Info] Downloading $3"
+	
 	FILE_NAME=""
 	if [ "$3" != "sha1sums" ]; then
 		FILE_NAME="$1wiki-$2-$3.sql.gz"
@@ -22,6 +18,25 @@ function download_file() {
 	URL="https://dumps.wikimedia.your.org/$1wiki/$2"
 	echo "Downloading $FILE_NAME"
 	time wget -P "$CUR_DIR/$OUT_DIR" "$URL/$FILE_NAME"
+	if ["$3" != "sha1sums" ]; then
+		time grep $FILE_NANME "$CUR_DIR/$OUT_DIR/$1wiki-$2-sha1sums.txt" | sha1sum -c -
+		if [ $? -nq 0 ]; then 
+			echo "[Warning] Checksum failed for '$3'"
+		fi
+	fi 
 }
 
-download_file $1 $2 $3
+if [ "$#" -eq 2 ]; then
+	download_file $1 $2 "sha1sums"
+	download_file $1 $2 "pagelinks"
+	download_file $1 $2 "page"
+	download_file $1 $2 "redirect"
+elif [ "$#" -eq 3 ]; then 
+	download_file $1 $2 $3
+elif [ "$#" -eq 1 ]; then 
+	echo "[Warrning] To be implemented"
+else 
+	echo "[Warrning] Error: Invalid number of arguments"
+	exit 1
+fi
+
