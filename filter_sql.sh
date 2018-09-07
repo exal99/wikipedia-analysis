ff4fa0f43c69e3da8347d019c6b0698a574000ef
Alexander Simko <alexander@simko.se>
2018-09-07 14:11:26 +0200

    Removed unnecessary lines and added echo at the end

--- filter_sql.sh
+++ filter_sql.sh
@@ -8,11 +8,4 @@ gunzip --stdout < "database/$1wiki-$2-page.sql.gz" \
 	| tee >(perl -ne 'print STDERR "\rCurrent id: $1       " if /^(\d+)/') \
 	| gzip -c --fast > "database/$1wiki-$2-page.sql.gz.tmp"
 
-		
-#	| gzip -c > "database/$1wiki-$2-page.sql.gz.tmp"
-	
-
-
-# 30668,0,'The_Lord\'s_Supper','',331,1,0,0.586246820712692,'20180722160246',NULL,565580949,23,'wikitext',NULL
-
-# 27306,0,'Seychelles_People\'s_Defence_Force','',58,0,0,0.435548723149624,'20180727203721','20180727203721',852281365,4701,'wikitext',NULL
+echo
