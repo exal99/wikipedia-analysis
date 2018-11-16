65533c9b253f4e8654ce3c540724b3451f5021f0
Alexander Simko <alexander.ch.simko@gmail.com>
2018-11-16 14:41:58 +0100

    Moved index definition to after insert statements

--- sql/pagelinks_definition.sql
+++ sql/pagelinks_definition.sql
@@ -3,5 +3,5 @@ CREATE TABLE pagelinks(
 	pl_target UNSIGNED int(10) NOT NULL
 );
 
-CREATE INDEX pl_from_index ON pagelinks (pl_from);
-CREATE INDEX pl_target_index ON pagelinks (pl_target); 
+-- CREATE INDEX pl_from_index ON pagelinks (pl_from);
+-- CREATE INDEX pl_target_index ON pagelinks (pl_target); 
