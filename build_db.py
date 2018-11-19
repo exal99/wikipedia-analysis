535166b5fca1188dc2e34b0a89e4ba1390390fd3
Alexander Simko <alexander.ch.simko@gmail.com>
2018-11-19 11:42:50 +0100

    Removed unnessesary lines

--- build_db.py
+++ build_db.py
@@ -1,11 +1,12 @@
 import sqlite3
 import gzip
 import time
+import sys
 from functools import wraps
 
 def measure_speed(iterator):
 	"""
-	Speed measurement decorator
+	Iterator speed measurement decorator
 	"""
 	@wraps(iterator)
 	def wrapper(*args, **kwargs):
@@ -38,17 +39,9 @@ def redirect_iterator(redirect_file):
 		yield (rd_from, rd_target)
 
 def insert_into_redirect(connection, language_code, date):
-	#cursor = connection.cursor()
 	print('[Info] Building \'redirect\' table')
 	with gzip.open(f'database/{language_code}wiki-{date}-redirect.sql.gz') as redirect:
 		connection.executemany('INSERT INTO redirect VALUES (?,?);', redirect_iterator(redirect))
-		#for rd_from, rd_target in map(lambda x: x[:-1].split(','), map(bytes.decode, redirect)):
-		#	try:
-		#		cursor.execute('INSERT INTO redirect VALUES (?,?);', (rd_from, rd_target))
-		#	except sqlite3.IntegrityError:
-		#		cursor.execute('UPDATE redirect SET rd_target = ? WHERE rd_from = ?;', (rd_target, rd_from))
-		#	if int(rd_from) % 1009 == 0:
-		#		print(f'\r[Info] Current id: {rd_from}', end='')
 	connection.commit()
 
 @measure_speed
@@ -57,23 +50,16 @@ def pagelinks_iterator(pagelinks_file):
 		yield (pl_from, pl_target)
 
 def insert_into_pagelinks(connection, language_code, date):
-	#cursor = connection.cursor()
 	print('[Info] Building \'pagelinks\' table')
 	with gzip.open(f'database/{language_code}wiki-{date}-pagelinks.sql.gz') as pagelinks:
 		connection.executemany('INSERT INTO pagelinks VALUES (?,?);', pagelinks_iterator(pagelinks))
-
-		#for ind, [pl_from, pl_target] in enumerate(map(lambda x: x[:-1].split(','), map(bytes.decode, pagelinks))):
-		#	try:
-		#		cursor.execute('INSERT INTO pagelinks VALUES (?,?);', (pl_from, pl_target))
-		#	except sqlite3.IntegrityError:
-		#		cursor.execute('UPDATE pagelinks SET pl_target = ? WHERE pl_from = ?;', (pl_target, pl_from))
-		#	if ind % 50077 == 0:
-		#		print(f'\r[Info] Current target: {pl_target}', end='')
 	print("[Info] Creating index", end='')
+	sys.stdout.flush()
 	connection.commit()
-	connection.execute("CREATE INDEX pl_from_index on pagelinks (pl_from);")
+	connection.execute("CREATE INDEX pl_from_index on pagelinks (pl_from);") # creates index after insert for faster build time
 	connection.execute("CREATE INDEX pl_target_index on pagelinks (pl_target);")
 	print("    Done")
+	sys.stdout.flush()
 
 def _split_page_line(line):
 	page_id          = line.split(',')[0]
@@ -88,19 +74,9 @@ def page_iterator(page_file):
 		yield (page_id, page_title, page_is_redirect)
 
 def insert_into_page(connection, language_code, date):
-	cursor = connection.cursor()
 	print('[Info] Building \'page\' table')
 	with gzip.open(f'database/{language_code}wiki-{date}-page.sql.gz') as page:
 		connection.executemany('INSERT INTO page VALUES (?,?,?);', page_iterator(page))
-		#for page_id, page_title, page_is_redirect in map(_split_page_line, map(bytes.decode, page)):
-		#	try:
-		#		cursor.execute('INSERT INTO page VALUES (?,?,?);', (page_id, page_title, page_is_redirect))
-		#	except sqlite3.IntegrityError:
-		#		cursor.execute('UPDATE page SET page_title = ?, page_is_redirect = ? WHERE page_id = ?;',
-		#			           (page_title, page_is_redirect, page_id))
-
-		#	if int(page_id) % 1009 == 0:
-		#		print(f'\r[Info] Current id: {page_id}', end='')
 	connection.commit()
 
 
