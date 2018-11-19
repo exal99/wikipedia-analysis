439d0eb1db7ce599f303d016cbf07896cc5b7522
Alexander Simko <alexander.ch.simko@gmail.com>
2018-11-19 21:11:51 +0100

    Working BFS search

--- search_path.py
+++ search_path.py
@@ -1,33 +1,45 @@
 import sqlite3
-import queue
 import collections
 
 def BFS(connection, from_article, to_article):
-	to_visit = queue.Queue()
-	visited  = set()
+	to_visit = collections.deque()
+	visited  = {}
 
-	to_visit.put(from_article)
-	visited.add(from_article)
+	to_visit.append(from_article)
+	visited[from_article] = 0
 
-	path = collections.defaultdict(list)
-	path[from_article].append((None,0))
+	path = collections.defaultdict(set)
+	path[from_article].add(None)
 
-	max_distance = -1
-
+	max_distance = 0
+	total_visited = 0
 	current_article = None
 
-	while not to_visit.empty() and (max_distance == -1 or path[current_article][0][1] <= max_distance):
+	while to_visit:
 
-		current_article = to_visit.get()
-
+		current_article = to_visit.popleft()
+		total_visited += 1
+		#print(f"\r{visited[current_article]} {total_visited}", end='')
 		if current_article == to_article:
-			max_distance = path[to_article][0][1]
+			return path
 
 		for (linked_article,) in connection.execute('SELECT pl_target FROM pagelinks WHERE pl_from=?;', (current_article,)):
-			#print(linked_article)
 			if linked_article not in visited:
-				to_visit.put(linked_article)
-				visited.add(linked_article)
-				path[linked_article].append((current_article, path[current_article][0][1] + 1))
-				#print(linked_article)
+				to_visit.append(linked_article)
+				visited[linked_article] = visited[current_article] + 1
+				path[linked_article].add(current_article)
+
+			elif visited[current_article] + 1 == visited[linked_article]:
+				path[linked_article].add(current_article)
+
+			if visited[linked_article] > max_distance:
+				max_distance = visited[linked_article]
+
+		
 	return path
+
+def main():
+	c=sqlite3.connect('database/enwiki.db')
+	b=BFS(c, 37817,20127)
+
+main()
