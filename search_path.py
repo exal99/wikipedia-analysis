171215b4fd11e4732ef615bcd18e648fec81024f
Alexander Simko <alexander.ch.simko@gmail.com>
2018-11-24 11:28:55 +0100

    Some code cleanup, bugfixes and code improvements

--- search_path.py
+++ search_path.py
@@ -2,43 +2,6 @@ import sqlite3
 import collections
 import itertools
 
-def BFS(connection, from_article, to_article):
-	to_visit = collections.deque()
-	visited  = {}
-
-	to_visit.append(from_article)
-	visited[from_article] = 0
-
-	path = collections.defaultdict(set)
-	path[from_article].add(None)
-
-	max_distance = 0
-	total_visited = 0
-	current_article = None
-
-	while to_visit:
-
-		current_article = to_visit.popleft()
-		total_visited += 1
-		#print(f"\r{visited[current_article]} {total_visited}", end='')
-		if current_article == to_article:
-			return path
-
-		for (linked_article,) in connection.execute('SELECT pl_target FROM pagelinks WHERE pl_from=?;', (current_article,)):
-			if linked_article not in visited:
-				to_visit.append(linked_article)
-				visited[linked_article] = visited[current_article] + 1
-				path[linked_article].add(current_article)
-
-			elif visited[current_article] + 1 == visited[linked_article]:
-				path[linked_article].add(current_article)
-
-			if visited[linked_article] > max_distance:
-				max_distance = visited[linked_article]
-
-		
-	return path
-
 def bidirectional_BFS(connection, from_article, to_article):
 	source_queue = collections.deque((from_article,))
 	target_queue = collections.deque((to_article,))
@@ -58,15 +21,10 @@ def bidirectional_BFS(connection, from_article, to_article):
 
 	intersections = set()
 
-	time_run = 0
-
 	while source_queue and target_queue:
-		time_run += 1
-		print(f'\r',max(source_visited.values()), max(target_visited.values()),end='')
 		if found_path and (source_visited[source_queue[0]] > source_deapth and target_visited[target_queue[0]] > target_deapth):
-			intersections = target_visited.keys() & source_visited.keys()
-			print('\n', max(source_visited.values()), max(target_visited.values()))
-			return source_parent, target_parent, intersections, source_visited, target_visited
+			intersections = filter_intersections(source_visited, target_visited, intersections)
+			return construct_path(source_parent, target_parent, intersections)
 
 		if found_path and source_visited[source_queue[0]] > source_deapth:
 			target_added, t_deapth = BFS_target(target_queue, target_visited, target_parent, connection)
@@ -78,15 +36,6 @@ def bidirectional_BFS(connection, from_article, to_article):
 			source_added, s_deapth = BFS_source(source_queue, source_visited, source_parent, connection)
 			target_added, t_deapth = BFS_target(target_queue, target_visited, target_parent, connection)
 
-		#if not found_path and source_added & target_visited.keys(): # Path from source intersects path from target
-		#	found_path = True
-		#	source_deapth = s_deapth
-		#	target_deapth = t_deapth
-		#elif not found_path and target_added & source_visited.keys(): # Path from target intersects path from source
-		#	found_path = True
-		#	source_deapth = s_deapth
-		#	target_deapth = t_deapth
-
 			intersections |= source_added & target_visited.keys()
 			intersections |= target_added & source_visited.keys()
 
@@ -94,31 +43,40 @@ def bidirectional_BFS(connection, from_article, to_article):
 			found_path = True
 			source_deapth = s_deapth
 			target_deapth = t_deapth
-			print(s_deapth, t_deapth, len(source_queue), len(target_queue))
 
 	if found_path:
-		return construct_path(source_parent, target_parent, from_article, to_article, intersections)
+		return construct_path(source_parent, target_parent, intersections)
 	else:
 		return None
 
-def construct_path(source_parent, target_parent, source, target, intersections):
+def filter_intersections(source_visited, target_visited, intersections):
+	filterd = set()
+	shortest = float('inf')
+	for intersection in intersections:
+		if source_visited[intersection] + target_visited[intersection] < shortest:
+			shortest = source_visited[intersection] + target_visited[intersection]
+			filterd = {intersection}
+		elif source_visited[intersection] + target_visited[intersection] == shortest:
+			filterd.add(intersection)
+	return filterd
+
+def construct_path(source_parent, target_parent, intersections):
 	paths = set()
 
 	for intersection in intersections:
-		from_source_to_inter = construct_path_from_to(source_parent, source, intersection)
-		from_inter_to_target = construct_path_from_to(target_parent, target, intersection, reverse=True)
-		print(from_source_to_inter,"-------", from_inter_to_target)
+		from_source_to_inter = construct_path_from_to(source_parent, intersection)
+		from_inter_to_target = construct_path_from_to(target_parent, intersection, reverse=True)
 		for path in itertools.product(from_source_to_inter, from_inter_to_target):
 			paths.add(tuple(path[0] + path[1][1:]))
-	return paths
+	return list(paths)
 		
-def construct_path_from_to(source_parent, target, start, reverse=False):
-	if start == target:
-		return [[target]]
+def construct_path_from_to(source_parent, start, reverse=False):
+	if None in source_parent[start]:
+		return [[start]]
 
 	paths = []
 	for parent in source_parent[start]:
-		for path in construct_path_from_to(source_parent, target, parent, reverse):
+		for path in construct_path_from_to(source_parent, parent, reverse):
 			if reverse:
 				path.insert(0, start)
 			else:
@@ -158,6 +116,16 @@ def BFS_target(queue, visited, parent, connection):
 			parent[linked_article].add(current)
 
 	return added_articles, visited[current]
+
+def get_id_of_title(connection, title):
+	page_id, is_redirect = connection.execute(f"SELECT page_id, page_is_redirect FROM page WHERE page_title='''{title}'''").fetchone()
+	if is_redirect:
+		(page_id,) = connection.execute("SELECT rd_target FROM redirect WHERE rd_from=?", (page_id,)).fetchone()
+	return page_id
+
+def get_title_of_id(connection, page_id):
+	return connection.execute('SELECT page_title FROM page WHERE page_id=?',(page_id,)).fetchone()
+
 
 def main():
 	c=sqlite3.connect('database/enwiki.db')
