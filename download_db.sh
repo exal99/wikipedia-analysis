d41076ea566aa5e3cba36eed7fc138f4ed43668a
Alexander Simko <alexander.ch.simko@gmail.com>
2018-12-29 10:37:55 +0100

    Implemented automatic download of latest database

--- download_db.sh
+++ download_db.sh
@@ -18,15 +18,20 @@ function download_file() {
 	URL="https://dumps.wikimedia.your.org/$1wiki/$2"
 	echo "Downloading $FILE_NAME"
 	wget -q --show-progress -P "$CUR_DIR/$OUT_DIR" "$URL/$FILE_NAME"
-	if [ "$3" != "sha1sums" ]; then
-		echo "[Info] Checking SHA-Checksum on $FILE_NAME"
-		cd "$OUT_DIR"
-		grep $FILE_NAME "$1wiki-$2-sha1sums.txt" | sha1sum -c -
-		cd ..
-		if [ $? -ne 0 ]; then 
-			echo "[Warning] Checksum failed for '$3'"
-		fi
-	fi 
+	echo
+}
+
+function check_sha1sum() {
+	echo "[Info] Checking SHA-Checksum"
+	cd "$OUT_DIR"
+	sha1sum -c --ignore-missing $1wiki-$2-sha1sums.txt
+	if [ $? -ne 0 ]; then 
+		echo "[Warning] Checksum failed"
+	fi
+	cd ..
+	echo "[Info] SHA-Checksum Compleat"
+	echo
+
 }
 
 if [ "$#" -eq 2 ]; then
@@ -34,10 +39,17 @@ if [ "$#" -eq 2 ]; then
 	download_file $1 $2 "pagelinks"
 	download_file $1 $2 "page"
 	download_file $1 $2 "redirect"
+	check_sha1sum $1 $2
 elif [ "$#" -eq 3 ]; then 
 	download_file $1 $2 $3
+	check_sha1sum $1 $2
 elif [ "$#" -eq 1 ]; then 
-	echo "[Warrning] To be implemented"
+	download_file $1 "latest" "sha1sums"
+	DATE=`sed -rn "s/^.* enwiki-([0-9]+)-.*$/\1/p" "database/$1wiki-latest-sha1sums.txt" | head -n 1 -q` 
+	download_file $1 $DATE "pagelinks"
+	download_file $1 $DATE "page"
+	download_file $1 $DATE "redirect"
+	check_sha1sum $1 "latest"
 else 
 	echo "[Error] Invalid number of arguments"
 	exit 1
