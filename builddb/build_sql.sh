9241b25cce0cf9a7a91dd79ea67919ba90042ead
Alexander Simko <alexander.ch.simko@gmail.com>
2019-01-03 14:06:24 +0100

    Removed hardcoded database names

--- builddb/build_sql.sh
+++ builddb/build_sql.sh
@@ -3,27 +3,27 @@
 createdb $1wikidb
 
 echo "[Info] Creating page table"
-psql -f "sql/page_definition.sql" wikidb
+psql -f "sql/page_definition.sql"  $1wikidb
 
 pigz -dc < "database/$1wiki-$2-page.sql.gz" \
-    | psql -c "COPY page FROM STDIN WITH DELIMITER E'\t' QUOTE '''' ESCAPE '\' CSV;" wikidb
+    | psql -c "COPY page FROM STDIN WITH DELIMITER E'\t' QUOTE '''' ESCAPE '\' CSV;"  $1wikidb
 
 echo "Done"
 echo
 
 
 echo "[Info] Creating redirect table"
-psql -f "sql/redirect_definition.sql" wikidb
+psql -f "sql/redirect_definition.sql"  $1wikidb
 pigz -dc < "database/$1wiki-$2-redirect.sql.gz" \
-    | psql -c "COPY redirect FROM STDIN WITH DELIMITER E'\t' QUOTE '''' ESCAPE '\' CSV;" wikidb
+    | psql -c "COPY redirect FROM STDIN WITH DELIMITER E'\t' QUOTE '''' ESCAPE '\' CSV;"  $1wikidb
 
 echo "Done"
 echo
 
 
 echo "[Info] Creating links table"
-psql -f "sql/links_definition.sql" wikidb
+psql -f "sql/links_definition.sql"  $1wikidb
 ./create_pagelinks.py "database/$1wiki-$2-pagelinks_f_id.sql.gz" "database/$1wiki-$2-pagelinks_t_id.sql.gz" \
-    | psql -c "COPY links FROM STDIN WITH DELIMITER E'\t' QUOTE '''' ESCAPE '\' CSV;" wikidb
+    | psql -c "COPY links FROM STDIN WITH DELIMITER E'\t' QUOTE '''' ESCAPE '\' CSV;"  $1wikidb
 
-psql -f "sql/paths_definition.sql" wikidb
+psql -f "sql/paths_definition.sql"  $1wikidb
