02464e97c0eec6f9f48ee706b479da0ca9f5a981
Alexander Simko <alexander.ch.simko@gmail.com>
2019-04-04 10:44:47 +0200

    Added some documentation

--- database.py
+++ database.py
@@ -1,8 +1,25 @@
+"""
+This module provides a nice wrapper around the psycopg2 module to interface
+with my databases.
+"""
+
+from typing import Tuple, Dict, List
+
 import psycopg2
 import random
 
 class WikiDatabase():
-	def __init__(self, database, **kwargs):
+	"""
+	A WikiDatabase object. Used to interface with the generated sql-database. This
+	class provieds some nice methods for interfacing with the database.
+	"""
+	def __init__(self, database: str, **kwargs) -> None:
+		"""
+		Creates a new WikiDatabase object.
+
+		Arguments:
+		database 	The name of the database.
+		"""
 		self.conn = psycopg2.connect(dbname=database, **kwargs)
 		self.cur = self.conn.cursor()
 		self.cur.execute('SELECT MIN(page_id) FROM page;')
@@ -11,41 +28,93 @@ class WikiDatabase():
 		self.max_id = self.cur.fetchone()[0]
 		self.last_starts_with = ()
 
-	def get_outgoing_count(self, outgoing):
+	def get_outgoing_count(self, outgoing: Tuple[int]) -> int:
+		"""
+		Returns the total number of outgoing links among the given articles.
+
+		Arguments:
+		outgoing 	A tuple of the article ids to examine.
+
+		Return:
+		The total number of outgoing links
+		"""
 		outgoing_str = format_tuple(outgoing)
 		self.cur.execute("SELECT SUM(outgoing_count) FROM links WHERE id IN %s;" % outgoing_str)
 		res = self.cur.fetchone()[0]
 		return res if res is not None else 0
 
-	def get_incoming_count(self, incoming):
+	def get_incoming_count(self, incoming: Tuple[int]) -> int:
+		"""
+		Returns the total number of incoming links among the given articles.
+
+		Arguments:
+		incoming 	A tuple of the article ids to examine.
+
+		Return:
+		The total number of incoming links
+		"""
 		incoming_str = format_tuple(incoming)
 		self.cur.execute("SELECT SUM(incoming_count) FROM links WHERE id IN %s;" % incoming_str)
 		res = self.cur.fetchone()[0]
 		return res if res is not None else 0
 
-	def get_incoming_links(self, incoming):
+	def get_incoming_links(self, incoming: Tuple[int]) -> Dict[int, List[int]]:
+		"""
+		Gets all the incoming links of the given list of articles.
+
+		Arguments:
+		incoming 	The articles that should have their ids fetched.
+
+		Returns:
+		A dictionary mapping article id to a list of all incoming links. 
+		"""
 		incoming_str = format_tuple(incoming)
 		self.cur.execute("SELECT id, incoming_links FROM links WHERE id IN %s;" % incoming_str)
 		return dict(self.cur.fetchall())
 
-	def get_outgoing_links(self, outgoing):
+	def get_outgoing_links(self, outgoing: Tuple[int]) -> Dict[int, List[int]]:
+		"""
+		See get_incoming_links!
+		"""
 		outgoing_str = format_tuple(outgoing)
 		self.cur.execute("SELECT id, outgoing_links FROM links WHERE id IN %s;" % outgoing_str)
 		return dict(self.cur.fetchall())
 
-	def get_id_of_title(self, title):
+	def get_id_of_title(self, title:str) -> int:
+		"""
+		Returns the id of all the given title. The title must be sanetized (i.e.
+		all spaces must be replaced by underscore).
+
+		Arguments:
+		title 	The title to get the id of.
+
+		Return:
+		The id of the given title.
+		"""
 		self.cur.execute("SELECT page_id FROM page WHERE page_title='%s';" % title)
 		res = self.cur.fetchone()
 		return res[0] if res else None
 
-	def get_titles_of_ids(self, ids):
+	def get_titles_of_ids(self, ids: Tuple[int]) -> List[str]:
+		"""
+		Gets all the titles of the given tuple of ids.
+
+		Arguments:
+		ids 	The ids of the articles to fetch
+
+		Return:
+		A list of all the article titles.
+		"""
 		ids_str = format_tuple(ids)
 		when_clause = "WHEN {} THEN {} " * len(ids) + "END"
 		when_clause = when_clause.format(*[f(x) for x in enumerate(ids) for f in (lambda x:x[1], lambda x:x[0])]) # see: https://stackoverflow.com/a/11869360/4820676
 		self.cur.execute("SELECT page_title FROM page WHERE page_id IN %s ORDER BY CASE page_id %s;" % (ids_str, when_clause))
 		return [result[0] for result in self.cur]
 
-	def get_random_page(self):
+	def get_random_page(self) -> int:
+		"""
+		Returns the id of a random article.
+		"""
 		page_id = 0
 		while page_id == 0:
 			page_id = random.randint(self.min_id, self.max_id)
@@ -55,7 +124,15 @@ class WikiDatabase():
 					return page_id
 			page_id = 0
 
-	def dump_statistics(self, sources, targets, all_paths):
+	def dump_statistics(self, sources: List[int], targets: List[int], all_paths: List[Tuple[int]]) -> None:
+		"""
+		Inserts the result of multiple searches into the database.
+
+		Arguments:
+		sources 		A list of all the source ids
+		targets 		A list of all the target ids
+		all_paths 		A tuple of an example path for each search
+		"""
 		values = ""
 		for source, target, paths in zip(sources, targets, all_paths):
 			if paths is not None:
@@ -66,7 +143,16 @@ class WikiDatabase():
 			self.cur.execute("INSERT INTO paths VALUES %s;" %values[:-1]) #removes the last comma
 			self.conn.commit()
 
-	def get_all_starts_with(self, text):
+	def get_all_starts_with(self, text: str) -> List[str]:
+		"""
+		Returns all (at most 'number_to_fetch') articles that start with 'text'.
+
+		Arguments:
+		text 	The begining of some article title
+
+		Return:
+		A list of all the articles begining with the given string.
+		"""
 		number_to_fetch = 50
 		if self.last_starts_with:
 			if text == self.last_starts_with[0]:
@@ -83,5 +169,18 @@ class WikiDatabase():
 
 
 
-def format_tuple(tup):
+def format_tuple(tup: Tuple) -> str:
+	"""
+	Formats the tuple to be used in an SQL-request. If a
+	python tuple only has one element it is represented as
+	'(1,)'. An SQL list with only one element however needs
+	too look like '(1)' therefore the last ',)' is replaced
+	by just a ')'
+
+	Arguments:
+	tup 	the tuple to be formated
+
+	Return:
+	The formated string of the tuple.
+	"""
 	return str(tup).replace(',)', ')')
--- pathfinder.py
+++ pathfinder.py
@@ -1,9 +1,29 @@
+"""
+This module contains all the tools for finding the shortes path
+between two articles and constructing the path between them.
+"""
+
+from typing import List, Tuple, DefaultDict, Set
+
 import database
 import collections
 import itertools
 
 
-def bidirectional_BFS(db, source, target): 
+def bidirectional_BFS(db : WikiDatabase, source : int, target : int) -> List[Tuple[int]]: 
+	"""
+	Finds all the shortest paths between the given 'source' and 'target'.
+	This function uses a bidirectional breath first search (BFS).
+
+	Params:
+	db 			The WikiDatabase object used for the search
+	source 		The id of the article to start from
+	target 		The id of the target article
+
+	Return:
+	All shortest path with the same length from 'source' to 'target'. Each element
+	of the returned list contains one path. The path is a tuple of all the article ids.
+	"""
 	source_queue = (source,)
 	target_queue = (target,)
 
@@ -59,7 +79,24 @@ def bidirectional_BFS(db, source, target):
 		return None
 
 
-def construct_path(source_parent, target_parent, intersections):
+def construct_path(source_parent: DefaultDict[int, Set[int]], target_parent: DefaultDict[int, Set[int]],
+	               intersections: Set[int]) -> List[Tuple[int]]:
+	"""
+	Constructs all paths given the parrent nodes from the forward search, the
+	backward search and all the intersection points of the two searches. The
+	parrent mapping is a mapping between a node and all possible ways to reach
+	that node.
+
+	Arguments:
+	source_parent 		A mapping between each node and all possible ways to reach it from
+						the forward search.
+	target_parent 		A mapping between each node and all possible ways to reach it from
+	 					the backwards search.
+	intersections 		All the intersection points of the two searches.
+
+	Return:
+	All the shortes paths between the start and the end
+	"""
 	paths = []
 
 	for intersection in intersections:
@@ -69,13 +106,27 @@ def construct_path(source_parent, target_parent, intersections):
 			paths.append(tuple(path[0] + path[1][1:]))
 	return paths
 		
-def construct_path_from_to(source_parent, start, reverse=False):
-	if None in source_parent[start]:
+def construct_path_from_to(parrent: DefaultDict[int, Set[int]], start: Set[int],
+	                       reverse: bool=False) -> List[List[int]]:
+	"""
+	Constructs all the paths from the 'start' list given the parrent mapping
+	recursivly. If 'reverse' is True the result is reversed.
+
+	Arguments:
+	parrent 		The parrent mapping
+	start 			A list of all start locations
+	reverse 		Reverse flag
+
+	Return:
+	All paths from each node in the 'start' list to the end in the parrent,
+	i.e. the node wich have 'None' as parrent.
+	"""
+	if None in parrent[start]:
 		return [[start]]
 
 	paths = []
-	for parent in source_parent[start]:
-		for path in construct_path_from_to(source_parent, parent, reverse):
+	for parent in parrent[start]:
+		for path in construct_path_from_to(parrent, parent, reverse):
 			if reverse:
 				path.insert(0, start)
 			else:
